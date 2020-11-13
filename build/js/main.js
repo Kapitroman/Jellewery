f339c0cf4867188b4cd1ecc977b8204f62af8b7c
Kapitonov Roman <kapitroman@yandex.ru>
2020-11-13 07:49:39 +0300

    fonts

---
+++ build/js/main.js
@@ -0,0 +1,29 @@
+'use strict';
+
+(function() {
+
+  if (!Element.prototype.matches) {
+    Element.prototype.matches = Element.prototype.matchesSelector ||
+      Element.prototype.webkitMatchesSelector ||
+      Element.prototype.mozMatchesSelector ||
+      Element.prototype.msMatchesSelector;
+  }
+
+})();
+
+(function() {
+
+  if (!Element.prototype.closest) {
+
+    Element.prototype.closest = function(css) {
+      var node = this;
+
+      while (node) {
+        if (node.matches(css)) return node;
+        else node = node.parentElement;
+      }
+      return null;
+    };
+  }
+
+})();
